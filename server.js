b1a932ed5bd5c715fc4f860e21e3ba6b90d9fe82
sbahari <sbahari1090@gmail.com>
2021-03-04 11:17:46 +0700

    export app

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8909;
 
 app.get('/', (req, res) => {
-  res.json({ status: 200, message: 'Hello world' });
+  res.status(200).json({ status: 200, message: 'Hello world' });
 });
 
 app.listen(port, () => {
   console.log(`application running on http://localhost:${port}`);
 });
+
+module.exports = app;
